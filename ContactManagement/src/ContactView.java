54b19b20c261fa99fd4561abe928e18e668c5c4d
ananthayyaswamy <58972690+ananthayyaswamy@users.noreply.github.com>
2021-05-19 17:44:28 +0530

    Delete ContactManagement directory

--- ContactManagement/src/ContactView.java
+++
@@ -1,9 +0,0 @@
-
-public class ContactView {
-	public void show(Contact contact) {
-		System.out.println("firstname : " + contact.getFirstName());
-		System.out.println("lastname : " + contact.getLastName());
-		System.out.println("company :" + contact.getCompany());
-		System.out.println("dob : " + contact.getDob());
-	}
-}
